c0cb8df13ad868e829d47df097f278f9068f2a16
Simon <simonfong6@gmail.com>
2020-06-27 10:46:27 +0000

    added aws to flask

--- setup.py
+++ setup.py
@@ -5,17 +5,18 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name="template-python-flask",
+    name="applications",
     version="0.0.1",
     author="Simon Fong",
     author_email="simonfong6@gmail.com",
     description="A package.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/simonfong6/template-python-flask",
+    url="https://github.com/simonfong6/applications",
     packages=find_packages(),
     install_requires=[
         'flask >= 1.1.1',
+        'boto3 >= 1.12.0'
     ],
     python_requires='>=3.6',
     classifiers=[
