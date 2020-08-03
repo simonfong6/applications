0c702ac081edaed54722ea9c46372297778da0e0
Simon <simonfong6@gmail.com>
2020-08-03 09:49:23 +0000

    Add copy button

--- frontend/src/components/Company/Company.jsx
+++ frontend/src/components/Company/Company.jsx
@@ -1,14 +1,52 @@
 import React from 'react';
+import { MdContentCopy } from 'react-icons/md';
+import { FcCheckmark } from 'react-icons/fc';
+
+import './Company.css';
 
 
 class Company extends React.Component {
 
   constructor(props) {
     super(props);
+
+    this.state = {
+      copySucess: false
+    }
+  }
+
+  copyToClipboard(text) {
+    if (!document.hasFocus()) {
+      return;
+    }
+    navigator.clipboard.writeText(text);
+
+    this.setState({
+      copySucess: true
+    });
+
+    setTimeout(() => {
+      this.setState({
+        copySucess: false
+      });
+    }, 1000);
   }
 
   render() {
     let { name, career_link, auto_link } = this.props.company;
+
+    const {copySucess}  = this.state;
+
+    let icon;
+
+    if (copySucess) {
+      icon = <span className='ml-1'><FcCheckmark /></span>;
+    } else {
+      icon = (<span className='pointer ml-1'>
+       <MdContentCopy onClick={() => this.copyToClipboard(auto_link)}/>
+      </span>);
+    }
+    
 
     career_link = career_link || 'None';
     auto_link = auto_link || 'None';
@@ -16,7 +54,7 @@ class Company extends React.Component {
         <tr>
           <td>{name}</td>
           <td><a href={career_link}>{career_link}</a></td>
-          <td><a href={auto_link}>{auto_link}</a></td>
+    <td><a href={auto_link} id='copy'>{auto_link}</a>{icon}</td>
         </tr>
     );
   }
