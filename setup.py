2aa49ddd37af0b1b5eb446f99621fe7cda7817c0
Simon <simonfong6@gmail.com>
2020-08-05 01:45:46 +0000

    add dependencies

--- setup.py
+++ setup.py
@@ -16,7 +16,9 @@ setup(
     packages=find_packages(),
     install_requires=[
         'flask >= 1.1.1',
-        'boto3 >= 1.12.0'
+        'boto3 >= 1.12.0',
+        'requests >= 2.24.0'
+        'bcrypt >= 3.1.7'
     ],
     python_requires='>=3.6',
     classifiers=[
